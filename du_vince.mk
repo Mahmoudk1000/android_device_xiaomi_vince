aa61c2643cba2383d5a74c05f738d69df66f89b7
mahmoudk1000 <mahmoudk1000@gmail.com>
2020-07-08 17:14:52 +0000

    vince: inherit Corvus Parts

--- du_vince.mk
+++ du_vince.mk
@@ -23,6 +23,7 @@ $(call inherit-product, device/xiaomi/vince/device.mk)
 
 # Inherit some common Corvus-OS stuff.
 $(call inherit-product, vendor/du/config/common_full_phone.mk)
+$(call inherit-product, device/corvus/CorvusParts/corvusparts.mk)
 
 # official tag
 DU_BUILD_TYPE := OFFICIAL
